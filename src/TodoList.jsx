3fa6cd92be4bd315b10974a5a2df6b5151777ddf
CTDLisaHoo <265422412+CTDLisaHoo@users.noreply.github.com>
2026-07-19 18:22:46 -0400

    lesson-3-components

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,9 @@
+import TodoListItem from './TodoListItem.jsx';
 
-function TodoList() {
-    const todoList = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-]
-  
+function TodoList({todoList}) {
   return (
     <ul>
-            {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+      {todoList.map(todo => (<TodoListItem key={todo.id} todo={todo} />))}
     </ul>
   );
 }
