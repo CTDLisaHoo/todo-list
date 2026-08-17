96c5ad53413e461adcc3ad958ec1d4aa69f839ec
CTDLisaHoo <265422412+CTDLisaHoo@users.noreply.github.com>
2026-08-17 15:19:17 -0400

    Fix todo edit form and header

--- src/features/Todos/TodoList/TodoListItem.jsx
+++ src/features/Todos/TodoList/TodoListItem.jsx
@@ -53,8 +53,7 @@ function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
             </button>
 
             <button
-              type="button"
-              onClick={handleUpdate}
+              type="submit"
               disabled={!isValidTodoTitle(workingTitle)}
             >
               Update
