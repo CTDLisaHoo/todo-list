e94c04d818690ff8eaf1c3911a9bc8ba980ca37a
CTDLisaHoo <265422412+CTDLisaHoo@users.noreply.github.com>
2026-08-17 22:16:18 -0400

    Fix header

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,18 @@ import Logon from './features/Logon';
 import TodosPage from './features/Todos/TodosPage';
 
 function App() {
-  const [email, setEmail] = useState('');
+  //const [email, setEmail] = useState('');
   const [token, setToken] = useState('');
 
   return (
     <div>
-      <Header/>
+      <Header
+        email={email}
+        token={token}
+        onSetToken={setToken}
+        onSetEmail={setEmail}
+      />
+      
       {token ? (
         <TodosPage token={token} />
       ) : (
